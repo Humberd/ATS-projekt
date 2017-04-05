d62b1384cc56958c6e1bab7f1c76d570468fe13d
Humberd <sawik11@wp.pl>
2017-04-05 21:32:14 +0200

    Initial Commit

---
+++ EmptyGeneralTesting/UnitTesting/TestAST.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "TNode.h"
+#include <iostream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{		
+	TEST_CLASS(TestAST)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			TNode T;
+			// TODO: Your test code here
+			std::cout << "dupa";
+		}
+
+	};
+}
