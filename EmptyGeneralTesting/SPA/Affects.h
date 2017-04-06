7606bff88ebe6723df831028b1826bc23c712b47
zaba37 <zaba37@gmail.com>
2017-04-06 21:50:59 +0200

    dupa

--- EmptyGeneralTesting/SPA/Affects.h
+++ EmptyGeneralTesting/SPA/Affects.h
@@ -7,4 +7,5 @@ class Affects {
 	public:
 		vector<ASSIGN> getAffects(ASSIGN a, bool goDeep);
 		vector<ASSIGN> getAffectsBy(ASSIGN a, bool goDeep);
+		bool ifAffects(ASSIGN a1, ASSIGN a2, bool goDeep);
 };
