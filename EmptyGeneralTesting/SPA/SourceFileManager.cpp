207d9aa6c14845ca413dfecf8e46f8e560c4beae
Humberd <sawik11@wp.pl>
2017-04-11 19:29:53 +0200

    Hardly modified source file manager

--- EmptyGeneralTesting/SPA/SourceFileManager.cpp
+++ EmptyGeneralTesting/SPA/SourceFileManager.cpp
@@ -6,5 +6,13 @@ SourceFileManager::SourceFileManager(string filePath): filePath(filePath) {
 SourceFileManager::~SourceFileManager() {
 }
 
+void SourceFileManager::openFile() {
+//	inputStream.open()
+}
+
+void SourceFileManager::closeFile() {
+}
+
 void SourceFileManager::readFile() {
+	
 }
--- EmptyGeneralTesting/SPA/SourceFileManager.h
+++ EmptyGeneralTesting/SPA/SourceFileManager.h
@@ -1,12 +1,17 @@
 #pragma once
 #include <stddef.h>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 class SourceFileManager {
 private:
 	string filePath;
+	ifstream inputStream;
+
+	void openFile();
+	void closeFile();
 
 public:
 	explicit SourceFileManager(string filePath);
