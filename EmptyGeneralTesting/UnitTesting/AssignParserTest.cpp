bfb17da9e27dfcdb69f2cce8ad3c29bbe8a0c4f1
Humberd <sawik11@wp.pl>
2017-04-22 18:39:55 +0200

    Added assign invalid test cases

--- EmptyGeneralTesting/UnitTesting/AssignParserTest.cpp
+++ EmptyGeneralTesting/UnitTesting/AssignParserTest.cpp
@@ -10,6 +10,8 @@
 #include "../SPA/AssignParser.h"
 #include "../SPA/AssignNode.h"
 #include "../SPA/VariableNode.h"
+#include "../SPA/ParserException.h"
+#include "../SPA/Operators.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
@@ -20,6 +22,9 @@ TEST_CLASS(AssignParserTest) {
 
 		tokensList.push_back(new LexerToken(TokenKeys::NAME, "x", 1));
 		tokensList.push_back(new LexerToken(TokenKeys::SPECIAL_CHARACTER, SpecialCharacters::EQUALS_SIGN, 1));
+		tokensList.push_back(new LexerToken(TokenKeys::INTEGER, "5", 1));
+		tokensList.push_back(new LexerToken(TokenKeys::SPECIAL_CHARACTER, SpecialCharacters::SEMICOLON, 1));
+
 
 		auto iterator = tokensList.begin();
 		auto iteratorEnd = tokensList.end();
@@ -52,6 +57,58 @@ TEST_CLASS(AssignParserTest) {
 			delete token;
 		}
 		delete node , assParser , parsersRepository;
+	}
+
+	TEST_METHOD(AssignParser_parse_Invalid) {
+		vector<vector<LexerToken*>*> invalidCasesList;
+
+		vector<LexerToken*>* list = new vector<LexerToken*>();
+		//empty case
+		invalidCasesList.push_back(list);
+
+		/*cannot start with integer*/
+		list = new vector<LexerToken*>();
+		list->push_back(new LexerToken(TokenKeys::INTEGER, "5", 1));
+		list->push_back(new LexerToken(TokenKeys::SPECIAL_CHARACTER, SpecialCharacters::EQUALS_SIGN, 1));
+		invalidCasesList.push_back(list);
+
+		/*cannot start with open brace*/
+		list = new vector<LexerToken*>();
+		list->push_back(new LexerToken(TokenKeys::SPECIAL_CHARACTER, SpecialCharacters::OPEN_BRACE, 1));
+		list->push_back(new LexerToken(TokenKeys::SPECIAL_CHARACTER, SpecialCharacters::EQUALS_SIGN, 1));
+		invalidCasesList.push_back(list);
+
+		/*must have equals sign after the variable*/
+		list = new vector<LexerToken*>();
+		list->push_back(new LexerToken(TokenKeys::NAME, "x", 1));
+		invalidCasesList.push_back(list);
+
+		for (auto caseList : invalidCasesList) {
+			auto iterator = caseList->begin();
+			auto iteratorEnd = caseList->end();
+
+			ParsersRepository* parsersRepository = new ParsersRepository;
+			parsersRepository->expressionParser = new ExpressionParserMock(parsersRepository, iterator, iteratorEnd);
+
+			AssignParser* assignParser = new AssignParser(parsersRepository, iterator, iteratorEnd);
+
+			auto pointer = [assignParser] {
+				assignParser->parse();
+			};
+
+			Assert::ExpectException<ParserException>(pointer);
+
+			delete assignParser, parsersRepository;
+		}
+
+		for (auto caseList : invalidCasesList) {
+			for (auto token : *caseList) {
+				delete token;
+			}
+			delete caseList;
+		}
+
+		invalidCasesList.clear();
 	}
 
 };
