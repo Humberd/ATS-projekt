b9b4bc1facf6f711a9ceab5a506063524fbd7865
Humberd <sawik11@wp.pl>
2017-04-23 16:14:13 +0200

    Change stringstream to vector<string> in a StatementListParser, because I couldn't figure out how to prevent momory leaks

--- EmptyGeneralTesting/SPA/StatementListParser.cpp
+++ EmptyGeneralTesting/SPA/StatementListParser.cpp
@@ -24,57 +24,66 @@ Node* StatementListParser::parse() {
 	}
 	nextElement();
 
-	while (true) {
-		stringstream* errorsStream = new basic_stringstream<char>;
+	try {
+		while (true) {
+			vector<string> errorsList;
+			throwOnEOF();
 
-		throwOnEOF();
-
-		if ((*iterator)->isCloseBrace()) {
-			if (statementListNode->getChildren().size() != 0) {
-				nextElement();
-				delete errorsStream, statementListNode;
-				break;
-			} else {
-				throw ParserException(getClassName() + " - there need to be at least one Statement: " + (*iterator)->toString());
+			if ((*iterator)->isCloseBrace()) {
+				if (statementListNode->getChildren().size() != 0) {
+					nextElement();
+					break;
+				} else {
+					throw ParserException(getClassName() + " - there need to be at least one Statement: " + (*iterator)->toString());
+				}
 			}
+
+			try {
+				auto statementNode = parsersRepo->assignParser->parse();
+				statementListNode->addChild(statementNode);
+				continue;
+			} catch (ParserException& e) {
+				errorsList.push_back(e.what());
+				errorsList.push_back("\n");
+			}
+
+			try {
+				auto statementNode = parsersRepo->callParser->parse();
+				statementListNode->addChild(statementNode);
+				continue;
+			} catch (ParserException& e) {
+				errorsList.push_back(e.what());
+				errorsList.push_back("\n");
+			}
+
+			try {
+				auto statementNode = parsersRepo->whileParser->parse();
+				statementListNode->addChild(statementNode);
+				continue;
+			} catch (ParserException& e) {
+				errorsList.push_back(e.what());
+				errorsList.push_back("\n");
+			}
+
+			try {
+				auto statementNode = parsersRepo->ifParser->parse();
+				statementListNode->addChild(statementNode);
+				continue;
+			} catch (ParserException& e) {
+				errorsList.push_back(e.what());
+				errorsList.push_back("\n");
+			}
+
+			string errorsString = "";
+			for (auto error : errorsList) {
+				errorsString += error;
+			}
+
+			throw ParserException(getClassName() + " - " + errorsString);
 		}
-
-		try {
-			auto statementNode = parsersRepo->assignParser->parse();
-			statementListNode->addChild(statementNode);
-			continue;
-		} catch (ParserException& e) {
-			*errorsStream << e.what() << "\n";
-		}
-
-		try {
-			auto statementNode = parsersRepo->callParser->parse();
-			statementListNode->addChild(statementNode);
-			continue;
-		} catch (ParserException& e) {
-			*errorsStream << e.what() << "\n";
-		}
-
-		try {
-			auto statementNode = parsersRepo->whileParser->parse();
-			statementListNode->addChild(statementNode);
-			continue;
-		} catch (ParserException& e) {
-			*errorsStream << e.what() << "\n";
-		}
-
-		try {
-			auto statementNode = parsersRepo->ifParser->parse();
-			statementListNode->addChild(statementNode);
-			continue;
-		} catch (ParserException& e) {
-			*errorsStream << e.what() << "\n";
-		}
-
-		string errorsString = (*errorsStream).str();
-		delete errorsStream, statementListNode;
-
-		throw ParserException(getClassName() + " - " + errorsString);
+	} catch (ParserException&) {
+		delete statementListNode;
+		throw;
 	}
 
 	return statementListNode;
