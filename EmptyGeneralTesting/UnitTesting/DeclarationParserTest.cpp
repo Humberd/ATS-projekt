6ec95485492087454d00b9ae58e7cb2dd119337c
Humberd <sawik11@wp.pl>
2017-05-21 12:56:25 +0200

    Created invalid test case for declaration parser

--- EmptyGeneralTesting/UnitTesting/DeclarationParserTest.cpp
+++ EmptyGeneralTesting/UnitTesting/DeclarationParserTest.cpp
@@ -7,6 +7,7 @@
 #include "../SPA/QuerySpecialCharacters.h"
 #include "../SPA/DParsersRepository.h"
 #include "../SPA/DeclarationParser.h"
+#include "../SPA/QParserException.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
@@ -31,5 +32,70 @@ TEST_CLASS(DeclarationParserTest) {
 		Assert::IsNotNull(declaredVariable);
 		Assert::IsTrue(declaredVariable->getType() == DeclarationKeywords::ASSIGN);
 		Assert::IsTrue(declaredVariable->getName() == "a");
+
+		for (auto token : tokensList) {
+			delete token;
+		}
+		delete parsersRepository , declarationParser , declaredVariable;
+	}
+
+	TEST_METHOD(DeclarationParser_parse_Invalid) {
+		vector<vector<QLexerToken*>*> invalidCasesList;
+
+		vector<QLexerToken*>* list = new vector<QLexerToken*>();
+		//empty case
+		invalidCasesList.push_back(list);
+
+		/*Must end with semicolon*/
+		list = new vector<QLexerToken*>();
+		list->push_back(new QLexerToken(QTokenKeys::DECLARATION_KEYWORD, DeclarationKeywords::CALL));
+		list->push_back(new QLexerToken(QTokenKeys::NAME, "bar"));
+		invalidCasesList.push_back(list);
+
+		/*Must have a name, not the integer as a name*/
+		list = new vector<QLexerToken*>();
+		list->push_back(new QLexerToken(QTokenKeys::DECLARATION_KEYWORD, DeclarationKeywords::CALL));
+		list->push_back(new QLexerToken(QTokenKeys::INTEGER, "1234"));
+		list->push_back(new QLexerToken(QTokenKeys::SPECIAL_CHARACTER, QuerySpecialCharacters::SEMICOLON));
+		invalidCasesList.push_back(list);
+
+		/*Must have a proper type at the beginning*/
+		list = new vector<QLexerToken*>();
+		list->push_back(new QLexerToken(QTokenKeys::QUERY_KEYWORD, "foo hello type"));
+		list->push_back(new QLexerToken(QTokenKeys::NAME, "bar"));
+		list->push_back(new QLexerToken(QTokenKeys::SPECIAL_CHARACTER, QuerySpecialCharacters::SEMICOLON));
+		invalidCasesList.push_back(list);
+
+		/*Must have a declared name*/
+		list = new vector<QLexerToken*>();
+		list->push_back(new QLexerToken(QTokenKeys::DECLARATION_KEYWORD, DeclarationKeywords::ASSIGN));
+		list->push_back(new QLexerToken(QTokenKeys::SPECIAL_CHARACTER, QuerySpecialCharacters::SEMICOLON));
+		invalidCasesList.push_back(list);
+
+		for (auto caseList : invalidCasesList) {
+			auto iterator = caseList->begin();
+			auto iteratorEnd = caseList->end();
+
+			DParsersRepository* parsersRepository = new DParsersRepository;
+
+			DeclarationParser* declarationParser = new DeclarationParser(parsersRepository, iterator, iteratorEnd);
+
+			auto pointer = [declarationParser] {
+				declarationParser->parse();
+			};
+
+			Assert::ExpectException<QParserException>(pointer);
+
+			delete declarationParser, parsersRepository;
+		}
+
+		for (auto caseList : invalidCasesList) {
+			for (auto token : *caseList) {
+				delete token;
+			}
+			delete caseList;
+		}
+
+		invalidCasesList.clear();
 	}
 };
